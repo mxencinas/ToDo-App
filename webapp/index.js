18b1ade7ecf44ff78656d492f065920bf21fb109
Maxi <mxmencinas@gmail.com>
2025-12-04 10:45:59 -0300

    agreado de i18n incompleto

--- webapp/index.js
+++ webapp/index.js
@@ -7,6 +7,9 @@ sap.ui.define(["sap/ui/dom/includeStylesheet"], function (includeStylesheet) {
   sap.ui.core.mvc.XMLView.create({
     viewName: "artech-capacitacion-fiori.view.App",
   }).then(function (oView) {
+    oView.setModel(new sap.ui.model.resource.ResourceModel({ bundleName: "artech-capacitacion-fiori.i18n.i18n" }), "i18n");
+            // Ruta al archivo de propiedades de internacionalización (i18n)
+            // Asigna el modelo con el alias "i18n" para usarlo en la vista
     oView.placeAt("content");
   });
 });
